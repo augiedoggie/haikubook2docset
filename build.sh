e6e26969422f7f3d39f8d5458892db1c2661df2b
Chris Roberts <cpr420@gmail.com>
2024-05-26 14:31:59 -0600

    adjust index for struct and class variables

--- build.sh
+++ build.sh
@@ -6,45 +6,42 @@ if ! test -d haiku;then
 	git clone --depth=1 --filter=blob:none --sparse https://github.com/haiku/haiku.git
 fi
 
-cd haiku
+#cd haiku
 
-git sparse-checkout set --cone \
+(cd haiku && git sparse-checkout set --cone \
 	docs/user \
 	headers/os \
 	headers/posix \
 	headers/private \
-	src/kits/game
+	src/kits/game)
 
-rm -rf generated
+rm -rf haiku/generated
 
-mkdir -v generated
+mkdir haiku/generated
 
-cd docs/user
-
-sed -i Doxyfile \
+sed -i haiku/docs/user/Doxyfile \
 	-e 's,DISABLE_INDEX          = NO,DISABLE_INDEX          = YES,' \
 	-e 's,SEARCHENGINE           = YES,SEARCHENGINE           = NO,' \
 	-e 's,HIDE_UNDOC_MEMBERS     = YES,HIDE_UNDOC_MEMBERS     = NO,' \
 	-e 's,HIDE_UNDOC_CLASSES     = YES,HIDE_UNDOC_CLASSES     = NO,' \
 	-e 's,ENABLED_SECTIONS       =,ENABLED_SECTIONS       = INTERNAL,'
 
-doxygen
-
-cd ../../..
+echo
+echo "Running doxygen..."
+(cd haiku/docs/user && doxygen > doxygen.log 2>&1)
 
 rm -rf org.haiku.HaikuBook.docset
 
 echo
-echo "Creating docset..."
+echo "Running doxygen2docset..."
 
-doxygen2docset --doxygen haiku/generated/doxygen/html --docset .
+doxygen2docset --doxygen haiku/generated/doxygen/html --docset . > d2d.log
 
 echo
 echo "Copying docset metadata..."
 
-cp -afv meta.json icon*.png org.haiku.HaikuBook.docset
+cp -af meta.json icon*.png org.haiku.HaikuBook.docset
 
-## Modify sqlite index with namespace/class prefix for methods
 echo
 echo "Rewriting docset index..."
 
@@ -57,29 +54,48 @@ echo "BEGIN TRANSACTION;" > modifyindex.sql
 echo "UPDATE searchIndex SET type = 'Category' WHERE type = 'Data' and path like 'group\_%.html#' ESCAPE '\';" >> modifyindex.sql
 echo "UPDATE searchIndex SET type = 'Guide' WHERE type = 'Data' AND name like '%\_intro' ESCAPE '\';" >> modifyindex.sql
 
-IFS='|'
+## regexes to extract fully qualifiied name
+## will also need pattern substitution to replace '_1' with ':', like ${BASH_REMATCH[1]//_1/:}
+## and convert double underscore to single, like ${foo//__/_}
 classRegex="class(\w+)\.html"
+structRegex="struct(\w+)\.html"
 
-## prefix methods and templates with class and namespace
+IFS='|'
+
+## prefix methods, variables, and templates with class and namespace
 while read -r -a line; do
-	## extract fully qualified class name from html file
 	if [[ "${line[3]}" =~ $classRegex ]];then
-		## also substitute _1_1 in namespaces
-		echo "UPDATE searchIndex SET name='${BASH_REMATCH[1]//_1/:}::${line[1]}' WHERE id=${line[0]};" >> modifyindex.sql
+		newName="${BASH_REMATCH[1]//_1/:}"
+		echo "UPDATE searchIndex SET name = '${newName//__/_}::${line[1]}' WHERE id = ${line[0]};" >> modifyindex.sql
 	fi
-done <<< $(sqlite3 $DB_PATH "SELECT * FROM searchIndex WHERE type='Method' OR type = 'Type' AND path like 'class%'")
+done <<< $(sqlite3 $DB_PATH "SELECT * FROM searchIndex WHERE type = 'Method' OR type = 'Type' OR type = 'Variable' AND path like 'class%'")
 
 ## prefix namespaced classes
 while read -r -a line; do
-	## extract fully qualified class name from html file
 	if [[ "${line[3]}" =~ $classRegex ]];then
-		## also substitute _1_1 in namespaces
-		echo "UPDATE searchIndex SET name='${BASH_REMATCH[1]//_1/:}' WHERE id=${line[0]};" >> modifyindex.sql
+		newName="${BASH_REMATCH[1]//_1/:}"
+		echo "UPDATE searchIndex SET name = '${newName//__/_}' WHERE id = ${line[0]};" >> modifyindex.sql
 	fi
 done <<< $(sqlite3 $DB_PATH "SELECT * FROM searchIndex WHERE type = 'Class' AND path like 'class%\_1\_1%' ESCAPE '\';")
+
+## prefix namespaced structs and reset type to 'Struct'
+while read -r -a line; do
+	if [[ "${line[3]}" =~ $structRegex ]];then
+		newName="${BASH_REMATCH[1]//_1/:}"
+		echo "UPDATE searchIndex SET type = 'Struct', name = '${newName//__/_}' WHERE id = ${line[0]};" >> modifyindex.sql
+	fi
+done <<< $(sqlite3 $DB_PATH "SELECT * FROM searchIndex WHERE type = 'Class' AND path like 'struct%';")
+
+## prefix struct variables and functions
+while read -r -a line; do
+	if [[ "${line[3]}" =~ $structRegex ]];then
+		newName="${BASH_REMATCH[1]//_1/:}"
+		echo "UPDATE searchIndex SET name = '${newName//__/_}::${line[1]}' WHERE id = ${line[0]};" >> modifyindex.sql
+	fi
+done <<< $(sqlite3 $DB_PATH "SELECT * FROM searchIndex WHERE type = 'Variable' OR type = 'Function' AND path like 'struct%';")
 
 echo "COMMIT;" >> modifyindex.sql
 
 sqlite3 $DB_PATH < modifyindex.sql
 
-rm -f modifyindex.sql
+rm -f modifyindex.sql doxygen.log d2d.log
